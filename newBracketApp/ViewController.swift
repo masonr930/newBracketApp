f8bfa5e356209963e294cd4f918200a6d0216f63
Mason <mason.rozumny23@d155.org>
2023-02-08 07:41:53 -0600

    Initial Commit

---
+++ newBracketApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newBracketApp
+//
+//  Created by MASON ROZUMNY on 2/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
