6133ede8cd361d29239b2ac9ae0f0a6dc2440c8a
Mason <mason.rozumny23@d155.org>
2023-02-09 07:44:31 -0600

    ok UI actually done now

--- newBracketApp/CreatorViewController.swift
+++ newBracketApp/CreatorViewController.swift
@@ -7,18 +7,40 @@
 
 import UIKit
 
-class CreatorViewController: UIViewController {
+class CreatorViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
+    @IBOutlet weak var textfieldOutlet: UITextField!
     @IBOutlet weak var tableViewOutlet: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        tableViewOutlet.dataSource = self
+        tableViewOutlet.delegate = self
     }
     
     @IBAction func addTeamButton(_ sender: UIButton) {
+        if let x = textfieldOutlet.text {
+            
+            
+        }
     }
     
    
-
+    @IBAction func createButton(_ sender: UIButton) {
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
