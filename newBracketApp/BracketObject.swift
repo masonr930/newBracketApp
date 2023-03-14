a729444273d89ed485111f6cd5bc26cfa45ad9b3
James Durcan <jdurcan05@gmail.com>
2023-03-14 08:09:39 -0500

    Bracket object working

--- newBracketApp/BracketObject.swift
+++ newBracketApp/BracketObject.swift
@@ -24,13 +24,32 @@ public class BracketObject: Codable{
         self.rounds = rounds
     }
     
-//    init(dict: [String:Any]){
-//        self.title = dict["title"] as! String
-//       var dRounds = dict["rounds"]
-//        var dMathch = dRounds[0]
-//
-//
-//    }
+    init(dict: [String:Any]){
+        self.title = dict["title"] as! String
+        var rounds2: [RoundClass] = []
+        var matches2: [MatchupClass] = []
+        print("This is super fun")
+        print("\(dict["rounds"])")
+        var dRounds = dict["rounds"] as! [String: Any]
+        print("Do not read")
+        var matcher: MatchupClass!
+        for i in 0..<dRounds.count {
+            var dMatch = dRounds["\(i)"] as! [String: Any]
+            for j in 0..<dMatch.count{
+                print("Waddup looser")
+                matcher = MatchupClass(hTeam: dMatch["homeTeam"] as! String, aTeam: dMatch["awayteam"] as! String, hScore: 0, aScore: 0, match: dMatch["isMatch"] as! Bool)
+                matcher.winner = dMatch["winner"] as! Bool
+                matcher.winnerCheck = dMatch["winnerCheck"] as! Bool
+                matcher.hasTeams = dMatch["hasTeams"] as! Bool
+                matches2.append(matcher)
+                print(matcher.homeTeam)
+            }
+            rounds2.append(RoundClass(bMatches: matches2, brounds: i))
+        }
+        self.rounds = rounds2
+
+
+    }
 //homeTeam: String
 //var awayTeam: String
 //var homeScore: Int
